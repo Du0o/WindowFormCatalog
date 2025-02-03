e44a4b3460af0440b8114036ee557d02af5fa179
SaltyIce <136198930+Du0o@users.noreply.github.com>
2025-02-02 16:08:39 -0800

    Add files via upload

---
+++ BasicAI/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace BasicAI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picGreenB = new System.Windows.Forms.PictureBox();
+            this.picGreenF = new System.Windows.Forms.PictureBox();
+            this.picGreenR = new System.Windows.Forms.PictureBox();
+            this.picGreenL = new System.Windows.Forms.PictureBox();
+            this.picBlueR = new System.Windows.Forms.PictureBox();
+            this.picBlueL = new System.Windows.Forms.PictureBox();
+            this.picBlueF = new System.Windows.Forms.PictureBox();
+            this.picBlueB = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.enemy = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGO = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.projectile = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblscore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picGreenB
+            // 
+            this.picGreenB.BackColor = System.Drawing.Color.Transparent;
+            this.picGreenB.Image = ((System.Drawing.Image)(resources.GetObject("picGreenB.Image")));
+            this.picGreenB.Location = new System.Drawing.Point(18, 370);
+            this.picGreenB.Name = "picGreenB";
+            this.picGreenB.Size = new System.Drawing.Size(104, 112);
+            this.picGreenB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGreenB.TabIndex = 0;
+            this.picGreenB.TabStop = false;
+            this.picGreenB.Visible = false;
+            // 
+            // picGreenF
+            // 
+            this.picGreenF.BackColor = System.Drawing.Color.Transparent;
+            this.picGreenF.Image = ((System.Drawing.Image)(resources.GetObject("picGreenF.Image")));
+            this.picGreenF.Location = new System.Drawing.Point(12, 308);
+            this.picGreenF.Name = "picGreenF";
+            this.picGreenF.Size = new System.Drawing.Size(104, 112);
+            this.picGreenF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGreenF.TabIndex = 1;
+            this.picGreenF.TabStop = false;
+            this.picGreenF.Visible = false;
+            // 
+            // picGreenR
+            // 
+            this.picGreenR.BackColor = System.Drawing.Color.Transparent;
+            this.picGreenR.Image = ((System.Drawing.Image)(resources.GetObject("picGreenR.Image")));
+            this.picGreenR.Location = new System.Drawing.Point(12, 252);
+            this.picGreenR.Name = "picGreenR";
+            this.picGreenR.Size = new System.Drawing.Size(104, 112);
+            this.picGreenR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGreenR.TabIndex = 2;
+            this.picGreenR.TabStop = false;
+            this.picGreenR.Visible = false;
+            // 
+            // picGreenL
+            // 
+            this.picGreenL.BackColor = System.Drawing.Color.Transparent;
+            this.picGreenL.Image = ((System.Drawing.Image)(resources.GetObject("picGreenL.Image")));
+            this.picGreenL.Location = new System.Drawing.Point(18, 206);
+            this.picGreenL.Name = "picGreenL";
+            this.picGreenL.Size = new System.Drawing.Size(104, 112);
+            this.picGreenL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGreenL.TabIndex = 3;
+            this.picGreenL.TabStop = false;
+            this.picGreenL.Visible = false;
+            // 
+            // picBlueR
+            // 
+            this.picBlueR.BackColor = System.Drawing.Color.Transparent;
+            this.picBlueR.Image = ((System.Drawing.Image)(resources.GetObject("picBlueR.Image")));
+            this.picBlueR.Location = new System.Drawing.Point(122, 207);
+            this.picBlueR.Name = "picBlueR";
+            this.picBlueR.Size = new System.Drawing.Size(104, 112);
+            this.picBlueR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBlueR.TabIndex = 7;
+            this.picBlueR.TabStop = false;
+            this.picBlueR.Visible = false;
+            // 
+            // picBlueL
+            // 
+            this.picBlueL.BackColor = System.Drawing.Color.Transparent;
+            this.picBlueL.Image = ((System.Drawing.Image)(resources.GetObject("picBlueL.Image")));
+            this.picBlueL.Location = new System.Drawing.Point(122, 269);
+            this.picBlueL.Name = "picBlueL";
+            this.picBlueL.Size = new System.Drawing.Size(104, 112);
+            this.picBlueL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBlueL.TabIndex = 6;
+            this.picBlueL.TabStop = false;
+            this.picBlueL.Visible = false;
+            // 
+            // picBlueF
+            // 
+            this.picBlueF.BackColor = System.Drawing.Color.Transparent;
+            this.picBlueF.Image = ((System.Drawing.Image)(resources.GetObject("picBlueF.Image")));
+            this.picBlueF.Location = new System.Drawing.Point(122, 325);
+            this.picBlueF.Name = "picBlueF";
+            this.picBlueF.Size = new System.Drawing.Size(104, 112);
+            this.picBlueF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBlueF.TabIndex = 5;
+            this.picBlueF.TabStop = false;
+            this.picBlueF.Visible = false;
+            // 
+            // picBlueB
+            // 
+            this.picBlueB.BackColor = System.Drawing.Color.Transparent;
+            this.picBlueB.Image = ((System.Drawing.Image)(resources.GetObject("picBlueB.Image")));
+            this.picBlueB.Location = new System.Drawing.Point(128, 387);
+            this.picBlueB.Name = "picBlueB";
+            this.picBlueB.Size = new System.Drawing.Size(104, 112);
+            this.picBlueB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBlueB.TabIndex = 4;
+            this.picBlueB.TabStop = false;
+            this.picBlueB.Visible = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = ((System.Drawing.Image)(resources.GetObject("Player.Image")));
+            this.Player.Location = new System.Drawing.Point(100, 60);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(120, 120);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 8;
+            this.Player.TabStop = false;
+            // 
+            // enemy
+            // 
+            this.enemy.BackColor = System.Drawing.Color.Transparent;
+            this.enemy.Image = ((System.Drawing.Image)(resources.GetObject("enemy.Image")));
+            this.enemy.Location = new System.Drawing.Point(710, 340);
+            this.enemy.Name = "enemy";
+            this.enemy.Size = new System.Drawing.Size(120, 120);
+            this.enemy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy.TabIndex = 9;
+            this.enemy.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.progressBar1.ForeColor = System.Drawing.Color.LightCoral;
+            this.progressBar1.Location = new System.Drawing.Point(804, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(167, 23);
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(853, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ur health";
+            // 
+            // lblGO
+            // 
+            this.lblGO.AutoSize = true;
+            this.lblGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGO.Location = new System.Drawing.Point(374, 280);
+            this.lblGO.Name = "lblGO";
+            this.lblGO.Size = new System.Drawing.Size(223, 39);
+            this.lblGO.TabIndex = 12;
+            this.lblGO.Text = "GAME OVER";
+            this.lblGO.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(784, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 83);
+            this.label2.TabIndex = 13;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(639, 22);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(150, 39);
+            this.lblTime.TabIndex = 15;
+            this.lblTime.Text = "ggggggg";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // projectile
+            // 
+            this.projectile.AutoSize = true;
+            this.projectile.BackColor = System.Drawing.Color.Red;
+            this.projectile.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.projectile.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.projectile.Location = new System.Drawing.Point(775, 10);
+            this.projectile.Name = "projectile";
+            this.projectile.Size = new System.Drawing.Size(14, 12);
+            this.projectile.TabIndex = 16;
+            this.projectile.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(825, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "ur score";
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Location = new System.Drawing.Point(879, 60);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(60, 17);
+            this.lblscore.TabIndex = 18;
+            this.lblscore.Text = "ur score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.projectile);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblGO);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.enemy);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.picBlueR);
+            this.Controls.Add(this.picBlueL);
+            this.Controls.Add(this.picBlueF);
+            this.Controls.Add(this.picBlueB);
+            this.Controls.Add(this.picGreenL);
+            this.Controls.Add(this.picGreenR);
+            this.Controls.Add(this.picGreenF);
+            this.Controls.Add(this.picGreenB);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "AI Chase";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGreenL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlueB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picGreenB;
+        private System.Windows.Forms.PictureBox picGreenF;
+        private System.Windows.Forms.PictureBox picGreenR;
+        private System.Windows.Forms.PictureBox picGreenL;
+        private System.Windows.Forms.PictureBox picBlueR;
+        private System.Windows.Forms.PictureBox picBlueL;
+        private System.Windows.Forms.PictureBox picBlueF;
+        private System.Windows.Forms.PictureBox picBlueB;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox enemy;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGO;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label projectile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblscore;
+    }
+}
+
---
+++ BasicAI/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicAI
+{
+    //programmer: Teddy T
+    //Date:2/2/25
+    //program: AI Chase
+    public partial class Form1 : Form
+    {
+        // setting vars
+        System.Random r = new System.Random((int)System.DateTime.Now.Ticks);
+        private int xSpeed = 15;
+        private int ySpeed = 15;
+        private int dir;
+        private int score = 0;
+        private decimal timer = 45;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // this code runs every 0.1 secs
+        private void moveTarget()
+        {
+            // updating labels 
+            lblscore.Text = score.ToString();
+            timer -= 0.1m;
+            lblTime.Text = timer.ToString();
+            //move target horz and verts
+
+            if (Player.Location.X > enemy.Location.X)
+            {
+                enemy.Left += xSpeed;
+                enemy.Image = picGreenL.Image;
+            }
+            if (Player.Location.Y > enemy.Location.Y)
+            {
+                enemy.Top += ySpeed;
+                enemy.Image = picGreenF.Image;
+            }
+            if (Player.Location.Y < enemy.Location.Y)
+            {
+                enemy.Top -= ySpeed;
+                enemy.Image = picGreenB.Image;
+            }
+            if (Player.Location.X < enemy.Location.X)
+            {
+                enemy.Left -= xSpeed;
+                enemy.Image = picGreenR.Image;
+            }
+            if (Player.Location.X == enemy.Location.X)
+            {
+                enemy.Left -= xSpeed;
+                enemy.Image = picGreenR.Image;
+            }
+            // for ending game
+            if (progressBar1.Value == 1 || timer == 0)
+            {
+                timer1.Enabled = false;
+                lblGO.Visible = true;
+                Player.Visible = false;
+                enemy.Visible = false;
+                projectile.Enabled = false;
+                
+            }
+            if (enemy.Location.X > Player.Location.X - 60 && enemy.Location.X < Player.Location.X + 60 && enemy.Location.Y > Player.Location.Y - 60 && enemy.Location.Y < Player.Location.Y + 60)
+            {
+                progressBar1.Value -= 1;
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveTarget();
+        }
+        // ignore Form1_KeyDown onl Form1_KeyDown_1 works
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //this capture which key is pressed
+            Player.Text = e.KeyValue.ToString();
+            int whichKey = e.KeyValue;
+            //left Arrow
+            if (whichKey == 37)
+            {
+                Player.Left -= 10;
+            }
+            // right
+            else if (whichKey == 39)
+            {
+                Player.Left += 10;
+            }
+            // up
+            if (whichKey == 38)
+            {
+                Player.Top -= 10;
+                Player.Image = null;
+            }
+            //down
+            else if (whichKey == 40)
+            {
+                Player.Top += 10;
+            }
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            //this capture which key is pressed
+            Player.Text = e.KeyValue.ToString();
+            int whichKey = e.KeyValue;
+            //left Arrow
+            if (whichKey == 37)
+            {
+                Player.Left -= 10;
+                Player.Image = picBlueL.Image;
+            }
+            //Right Arrow
+            else if (whichKey == 39)
+            {
+                Player.Left += 10;
+                Player.Image = picBlueR.Image;
+            }
+            //Up Arrow
+            if (whichKey == 38)
+            {
+                Player.Top -= 10;
+                Player.Image = picBlueB.Image;
+            }
+            //Down Arrow
+            else if (whichKey == 40)
+            {
+                Player.Top += 10;
+                Player.Image = picBlueF.Image;
+            }
+            //fix OOB
+            if (Player.Location.X < 0)
+            {
+                Player.Left += 10;
+            }
+            if (Player.Location.X > 650)
+            {
+                Player.Left -= 10;
+            }
+            if (Player.Location.Y < 0)
+            {
+                Player.Top += 10;
+            }
+            if (Player.Location.Y > 370)
+            {
+                Player.Top -= 10;
+            }
+            // shooting controls
+            if (whichKey == 32)
+            {
+                projectile.Left = Player.Left +
+                    Player.Width / 2;
+                projectile.Top = Player.Top + 50;
+                timer2.Enabled = true;
+                if (Player.Image == picBlueL.Image) { dir = 1; }
+                if (Player.Image == picBlueR.Image) { dir = 2; }
+                if (Player.Image == picBlueB.Image) { dir = 3; }
+                if (Player.Image == picBlueF.Image) { dir = 4; }
+            }
+            
+            if (timer2.Enabled == false)
+            {
+                projectile.Left = Player.Left +
+                    Player.Width / 2;
+                projectile.Top = Player.Top + 50;
+            }
+        }
+        //every 0.1 secs
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            moveTarget();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            moveProjectile();
+        }
+        // for shooting mecs
+        private void moveProjectile()
+        {
+            // left
+            if (dir == 1)
+            {
+                projectile.Left -= 30;
+            }
+            //right
+            if (dir == 2)
+            {
+                projectile.Left += 30;
+            }
+            //up
+            if (dir == 3)
+            {
+                projectile.Top -= 30;
+            }
+            //down
+            if (dir == 4)
+            {
+                projectile.Top += 30;
+            }
+            //fix OOB
+            if (projectile.Location.X < 0)
+            {
+                timer2.Enabled = false;
+            }
+            if (projectile.Location.X > 650)
+            {
+                timer2.Enabled = false;
+            }
+            if (projectile.Location.Y < 0)
+            {
+                timer2.Enabled = false;
+            }
+            if (projectile.Location.Y > 370)
+            {
+                timer2.Enabled = false;
+            }
+            // when it hits enemy
+            if (projectile.Bounds.IntersectsWith(enemy.Bounds))
+            {
+                timer2.Enabled = false;
+                int NewX = r.Next(0, 650);
+                int NewY = r.Next(0, 370);
+                score += 1;
+                enemy.Left = NewX;
+                enemy.Top = NewY;
+            }
+        }
+    }
+}
