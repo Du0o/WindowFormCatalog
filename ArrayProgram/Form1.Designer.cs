27afd0695a364ee0372353a627cd78d67d8655bf
SaltyIce <136198930+Du0o@users.noreply.github.com>
2025-03-10 10:01:31 -0700

    Add files via upload

---
+++ ArrayProgram/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace ArrayProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbldisplay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbldisplay
+            // 
+            this.lbldisplay.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbldisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbldisplay.Location = new System.Drawing.Point(31, 29);
+            this.lbldisplay.Name = "lbldisplay";
+            this.lbldisplay.Size = new System.Drawing.Size(810, 344);
+            this.lbldisplay.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(31, 410);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(168, 56);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(869, 509);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lbldisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbldisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ ArrayProgram/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayProgram
+{
+    //Programmer: Teddy T
+    //Program: Array Program
+    //Date:3/5/25
+    public partial class Form1 : Form
+    {
+        //set up random number generator
+        System.Random r = new System.Random((int)
+        System.DateTime.Now.Ticks);
+        int[] Num = new int[100];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // run button code
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+        }
+        // happens every tick
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string theNumbers = "";
+            int highest = 0;
+            int lowest = 50;
+            int avarage = 0;
+            int avaragetotal = 0;
+            int odd = 0;
+            int even = 0;
+            //fill the array with random numbers
+            for (int i = 0; i < Num.Length; i++)
+            {
+                Num[i] = r.Next(1, 101);
+                avaragetotal += Num[i];
+            }
+            //find the highest number
+            for (int j = 0; j < Num.Length; j++)
+            {
+                if (Num[j] > highest)
+                {
+                    highest = Num[j];
+                }
+            }
+            for (int g = 0; g < Num.Length; g++)
+            {
+                if (Num[g] < lowest)
+                {
+                    lowest = Num[g];
+                }
+            }
+            for (int r = 0; r < Num.Length; r++)
+            {
+                if(Num[r] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+            // display to label
+            for (int n = 0; n < Num.Length; n++)
+            {
+                theNumbers += Num[n] + " ";
+            }
+            avarage = avaragetotal / Num.Length;
+            lbldisplay.Text = theNumbers + "\n" +
+            "Highest= " + highest +
+            "\nLowest= " + lowest +
+            "\nOdds= " + odd +
+            "\nEvens= " + even +
+            "\nAvarage= " + avarage;
+        }
+    }
+}
