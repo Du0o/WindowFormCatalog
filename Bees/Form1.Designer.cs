1326155daeac9e4befa14a1c82b87b010366d6c5
SaltyIce <136198930+Du0o@users.noreply.github.com>
2025-03-11 09:39:26 -0700

    Add files via upload

---
+++ Bees/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Bees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Hive = new System.Windows.Forms.PictureBox();
+            this.Flower = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Hive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hive
+            // 
+            this.Hive.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.Hive, "Hive");
+            this.Hive.Name = "Hive";
+            this.Hive.TabStop = false;
+            // 
+            // Flower
+            // 
+            this.Flower.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.Flower, "Flower");
+            this.Flower.Name = "Flower";
+            this.Flower.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.Controls.Add(this.Flower);
+            this.Controls.Add(this.Hive);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Hive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Hive;
+        private System.Windows.Forms.PictureBox Flower;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Bees/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bees
+{
+    //Programmer : Teddy T
+    //Program : Bee Simulator
+    //Date : 3/10/25
+    public partial class Form1 : Form
+    {
+        // global variable setup
+        List<PictureBox> BeeList = new List<PictureBox>();
+        int BeeCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // when spacebar pressed
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeyEventArgs key = e;
+            //dynamically make bees
+            if (key.KeyValue == 32)
+            {
+                if (BeeCount < 11)
+                {
+                    // making a bee
+                    BeeList.Add(new PictureBox());
+                    //put picture box on form
+                    this.Controls.Add(BeeList.ElementAt(BeeCount));
+                    BeeList.ElementAt(BeeCount).Height = 50;
+                    BeeList.ElementAt(BeeCount).Width = 50;
+                    BeeList.ElementAt(BeeCount).Image =
+                    Image.FromFile("Bee.png", true);
+                    BeeList.ElementAt(BeeCount).Left =
+                    Hive.Left + 220;
+                    BeeList.ElementAt(BeeCount).Top =
+                    Hive.Top + 200;
+                    BeeList.ElementAt(BeeCount).SizeMode =
+                    PictureBoxSizeMode.StretchImage;
+                    BeeCount++;
+                }
+            }
+        }
+        // code for bee func and movement
+        private void moveBee()
+        {
+           // when jitting flower
+            for (int m = 0; m < BeeList.Count; m++)
+            {
+                if (Flower.Bounds.IntersectsWith
+                (BeeList.ElementAt(m).Bounds))
+                {
+                    BeeList.ElementAt(m).Image =
+                    Image.FromFile("PBee.png", true);
+                    BeeList.ElementAt(m).Width = 51;
+                }
+            }
+            // Bee movement
+            for (int i = 0; i < BeeList.Count; i++)
+            {
+                if (BeeList.ElementAt(i).Width == 51)
+                {
+                    if (BeeList.ElementAt(i).Top > Hive.Top)
+                    {
+                        BeeList.ElementAt(i).Top -= 10;
+                    }
+                    if (BeeList.ElementAt(i).Left > Hive.Left)
+                    {
+                        BeeList.ElementAt(i).Left -= 10;
+                    }
+                }
+                else
+                {
+                    
+                    
+                    if (BeeList.ElementAt(i).Top > Flower.Top)
+                    {
+                        BeeList.ElementAt(i).Top -= 10;
+                    }
+                    if (BeeList.ElementAt(i).Left < Flower.Left)
+                    {
+                        BeeList.ElementAt(i).Left += 10;
+                    }
+                }
+                
+
+
+            }
+            // removing the bee when hitting hive
+            for (int m = 0; m < BeeList.Count; m++)
+            {
+                if (Hive.Bounds.IntersectsWith
+                (BeeList.ElementAt(m).Bounds))
+                {
+                    this.Controls.Remove(BeeList.ElementAt(m));
+                    BeeList.Remove(BeeList.ElementAt(m));
+                    BeeCount--;
+                    if (BeeCount <= 0)
+                    {
+                        BeeList = new List<PictureBox>();
+                        BeeCount = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        // timer tick
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveBee();
+        }
+    }
+}
