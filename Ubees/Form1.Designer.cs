68d1f26c8cd03c2655ae18a4504a51e291be5fcb
SaltyIce <136198930+Du0o@users.noreply.github.com>
2025-03-19 10:36:26 -0700

    Add files via upload

---
+++ Ubees/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Bees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Hive = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.Flower2 = new System.Windows.Forms.PictureBox();
+            this.Flower3 = new System.Windows.Forms.PictureBox();
+            this.Flower1 = new System.Windows.Forms.PictureBox();
+            this.NECTAR = new System.Windows.Forms.Label();
+            this.lblnectar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Hive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hive
+            // 
+            this.Hive.BackColor = System.Drawing.Color.Transparent;
+            this.Hive.Image = ((System.Drawing.Image)(resources.GetObject("Hive.Image")));
+            this.Hive.Location = new System.Drawing.Point(-17, 119);
+            this.Hive.Margin = new System.Windows.Forms.Padding(2);
+            this.Hive.Name = "Hive";
+            this.Hive.Size = new System.Drawing.Size(220, 254);
+            this.Hive.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Hive.TabIndex = 0;
+            this.Hive.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Flower2
+            // 
+            this.Flower2.BackColor = System.Drawing.Color.Transparent;
+            this.Flower2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Flower2.Location = new System.Drawing.Point(467, 256);
+            this.Flower2.Margin = new System.Windows.Forms.Padding(2);
+            this.Flower2.Name = "Flower2";
+            this.Flower2.Size = new System.Drawing.Size(106, 188);
+            this.Flower2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Flower2.TabIndex = 2;
+            this.Flower2.TabStop = false;
+            // 
+            // Flower3
+            // 
+            this.Flower3.BackColor = System.Drawing.Color.Transparent;
+            this.Flower3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Flower3.Location = new System.Drawing.Point(589, 256);
+            this.Flower3.Margin = new System.Windows.Forms.Padding(2);
+            this.Flower3.Name = "Flower3";
+            this.Flower3.Size = new System.Drawing.Size(106, 188);
+            this.Flower3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Flower3.TabIndex = 3;
+            this.Flower3.TabStop = false;
+            this.Flower3.Tag = "";
+            // 
+            // Flower1
+            // 
+            this.Flower1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Flower1.BackColor = System.Drawing.Color.Transparent;
+            this.Flower1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Flower1.Location = new System.Drawing.Point(348, 256);
+            this.Flower1.Margin = new System.Windows.Forms.Padding(2);
+            this.Flower1.Name = "Flower1";
+            this.Flower1.Size = new System.Drawing.Size(106, 188);
+            this.Flower1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Flower1.TabIndex = 4;
+            this.Flower1.TabStop = false;
+            // 
+            // NECTAR
+            // 
+            this.NECTAR.Location = new System.Drawing.Point(1, 0);
+            this.NECTAR.Name = "NECTAR";
+            this.NECTAR.Size = new System.Drawing.Size(56, 17);
+            this.NECTAR.TabIndex = 5;
+            this.NECTAR.Text = "NECTAR";
+            // 
+            // lblnectar
+            // 
+            this.lblnectar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblnectar.Location = new System.Drawing.Point(53, 0);
+            this.lblnectar.Name = "lblnectar";
+            this.lblnectar.Size = new System.Drawing.Size(56, 17);
+            this.lblnectar.TabIndex = 6;
+            this.lblnectar.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(730, 441);
+            this.Controls.Add(this.lblnectar);
+            this.Controls.Add(this.NECTAR);
+            this.Controls.Add(this.Flower1);
+            this.Controls.Add(this.Flower3);
+            this.Controls.Add(this.Flower2);
+            this.Controls.Add(this.Hive);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Bee Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Hive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Hive;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox Flower2;
+        private System.Windows.Forms.PictureBox Flower3;
+        private System.Windows.Forms.PictureBox Flower1;
+        private System.Windows.Forms.Label NECTAR;
+        private System.Windows.Forms.Label lblnectar;
+    }
+}
+
---
+++ Ubees/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bees
+{
+    //Programmer : Teddy T
+    //Program : Bee Simulator Upgrade
+    //Date : 3/19/25
+    public partial class Form1 : Form
+    {
+        // global variable setup
+        System.Random r = new System.Random((int)
+        System.DateTime.Now.Ticks);
+        List <PictureBox> BeeList = new List<PictureBox>();
+        PictureBox[] FlowerArray = new PictureBox[3];
+        int BeeCount = 0;
+        int nectar = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // setting flower array
+            FlowerArray[0] = Flower1;
+            FlowerArray[1] = Flower2;
+            FlowerArray[2] = Flower3;
+            for (int i = 0; i < 3; i++)
+            {
+                
+                switch (r.Next(1, 4))
+                {
+                    case 1:
+                        FlowerArray[i].Image = Image.FromFile("Flower.png", true);
+                        break;
+                    case 2:
+                        FlowerArray[i].Image = Image.FromFile("sonicFlower.png", true);
+                        break;
+                    case 3:
+                        FlowerArray[i].Image = Image.FromFile("fireFlower.png", true);
+                        break;
+                }
+                
+            }
+        }
+        // when spacebar pressed
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeyEventArgs key = e;
+            //dynamically make bees
+            if (key.KeyValue == 32)
+            {
+                if (BeeCount < 11)
+                {
+                    // making a bee
+                    int beeType = r.Next(1, 4);
+                    BeeList.Add(new PictureBox());
+                    //put picture box on form
+                    this.Controls.Add(BeeList.ElementAt(BeeCount));
+                    BeeList.ElementAt(BeeCount).BringToFront();
+                    BeeList.ElementAt(BeeCount).Left =
+                    Hive.Left + 220 + r.Next(1,500);
+                    BeeList.ElementAt(BeeCount).Top =
+                    Hive.Top + 200 + r.Next(-300, 300);
+                    BeeList.ElementAt(BeeCount).SizeMode =
+                    PictureBoxSizeMode.StretchImage;
+                    
+                    switch (r.Next(1, 4))
+                    {
+                        case 1:
+                            BeeList.ElementAt(BeeCount).Image = Image.FromFile("Bee.png", true);
+                            BeeList.ElementAt(BeeCount).Height = 50;
+                            BeeList.ElementAt(BeeCount).Width = 50;
+                            break;
+                        case 2:
+                            BeeList.ElementAt(BeeCount).Image = Image.FromFile("fireBee.png", true);
+                            BeeList.ElementAt(BeeCount).Height = 75;
+                            BeeList.ElementAt(BeeCount).Width = 75;
+                            break;
+                        case 3:
+                            BeeList.ElementAt(BeeCount).Image = Image.FromFile("electroBee.png", true);
+                            BeeList.ElementAt(BeeCount).Height = 100;
+                            BeeList.ElementAt(BeeCount).Width = 100;
+                            break;
+                    }
+                    switch (r.Next(1, 4))
+                    {
+                        case 1:
+                            BeeList.ElementAt(BeeCount).Tag = 0;
+
+                            break;
+                        case 2:
+                            BeeList.ElementAt(BeeCount).Tag = 1;
+                            break;
+                        case 3:
+                            BeeList.ElementAt(BeeCount).Tag = 2;
+                            break;
+                    }
+                    BeeCount++;
+                }
+            }
+        }
+        // code for bee func and movement
+        private void moveBee()
+        {
+           // when hitting flower
+            for (int m = 0; m < BeeList.Count; m++)
+            {
+                if (Flower1.Bounds.IntersectsWith(BeeList.ElementAt(m).Bounds) || Flower2.Bounds.IntersectsWith(BeeList.ElementAt(m).Bounds) || Flower3.Bounds.IntersectsWith(BeeList.ElementAt(m).Bounds))
+                {
+                    if (BeeList.ElementAt(m).Width == 50)
+                    {
+                        BeeList.ElementAt(m).Width = 51;
+                    }
+                    else if (BeeList.ElementAt(m).Width == 75)
+                    {
+                        BeeList.ElementAt(m).Width = 76;
+                    }
+                    else if (BeeList.ElementAt(m).Width == 100)
+                    {
+                        BeeList.ElementAt(m).Width = 101;
+                    }
+                }
+            }
+            // Bee movement
+            for (int i = 0; i < BeeList.Count; i++)
+            {
+                if (BeeList.ElementAt(i).Width == 51)
+                {
+                    BeeList.ElementAt(i).Image = Image.FromFile("PBee.png", true);
+                }
+                else if (BeeList.ElementAt(i).Width == 76)
+                {
+                    BeeList.ElementAt(i).Image = Image.FromFile("fireBeeA.png", true);
+                }
+                else if (BeeList.ElementAt(i).Width == 101)
+                {
+                    BeeList.ElementAt(i).Image = Image.FromFile("electroBeeA.png", true);
+                }
+                if (BeeList.ElementAt(i).Width == 51 || BeeList.ElementAt(i).Width == 76 || BeeList.ElementAt(i).Width == 101)
+                {
+                    if (BeeList.ElementAt(i).Top > Hive.Top)
+                    {
+                        BeeList.ElementAt(i).Top -= 5;
+                    }
+                    if (BeeList.ElementAt(i).Left > Hive.Left)
+                    {
+                        BeeList.ElementAt(i).Left -= 5;
+                    }
+
+                    if (BeeList.ElementAt(i).Top < Hive.Top)
+                    {
+                        BeeList.ElementAt(i).Top += 5;
+                    }
+                    if (BeeList.ElementAt(i).Left < Hive.Left)
+                    {
+                        BeeList.ElementAt(i).Left += 5;
+                    }
+                }
+                else
+                {
+
+                    switch (BeeList.ElementAt(i).Tag)
+                    {
+                        case 0:
+                            if (BeeList.ElementAt(i).Top > Flower1.Top)
+                            {
+                                BeeList.ElementAt(i).Top -= 5;
+                            }
+                            if (BeeList.ElementAt(i).Left < Flower1.Left)
+                            {
+                                BeeList.ElementAt(i).Left += 5;
+                            }
+
+                            if (BeeList.ElementAt(i).Top < Flower1.Top)
+                            {
+                                BeeList.ElementAt(i).Top += 5;
+                            }
+                            if (BeeList.ElementAt(i).Left > Flower1.Left)
+                            {
+                                BeeList.ElementAt(i).Image =
+                            Image.FromFile("PBee.png", true);
+                                BeeList.ElementAt(i).Left -= 5;
+                            }
+                            break;
+                        case 1:
+                            if (BeeList.ElementAt(i).Top > Flower2.Top)
+                            {
+                                BeeList.ElementAt(i).Top -= 5;
+                            }
+                            if (BeeList.ElementAt(i).Left < Flower2.Left)
+                            {
+                                BeeList.ElementAt(i).Left += 5;
+                            }
+
+                            if (BeeList.ElementAt(i).Top < Flower2.Top)
+                            {
+                                BeeList.ElementAt(i).Top += 5;
+                            }
+                            if (BeeList.ElementAt(i).Left > Flower2.Left)
+                            {
+                                
+                                BeeList.ElementAt(i).Left -= 5;
+                            }
+                            break;
+                        case 2:
+                            if (BeeList.ElementAt(i).Top > Flower3.Top)
+                            {
+                                BeeList.ElementAt(i).Top -= 5;
+                            }
+                            if (BeeList.ElementAt(i).Left < Flower3.Left)
+                            {
+                                BeeList.ElementAt(i).Left += 5;
+                            }
+
+                            if (BeeList.ElementAt(i).Top < Flower3.Top)
+                            {
+                                BeeList.ElementAt(i).Top += 5;
+                            }
+                            if (BeeList.ElementAt(i).Left > Flower3.Left)
+                            {
+                                BeeList.ElementAt(i).Image =
+                            Image.FromFile("PBee.png", true);
+                                BeeList.ElementAt(i).Left -= 5;
+                            }
+                            break;
+                    }
+
+                    
+                }
+                
+
+
+            }
+            // removing the bee when hitting hive
+            for (int m = 0; m < BeeList.Count; m++)
+            {
+                if (Hive.Bounds.IntersectsWith
+                (BeeList.ElementAt(m).Bounds))
+                {
+                    switch (BeeList.ElementAt(m).Tag)
+                    {
+                        case 0:
+                            nectar += 100;
+                            break;
+                        case 1:
+                            nectar += 250;
+                            break;
+                        case 2:
+                            nectar += 500;
+                            break;
+                    }
+                    lblnectar.Text = nectar.ToString();
+                    this.Controls.Remove(BeeList.ElementAt(m));
+                    BeeList.Remove(BeeList.ElementAt(m));
+                    BeeCount--;
+                    if (BeeCount <= 0)
+                    {
+                        BeeList = new List<PictureBox>();
+                        BeeCount = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        // timer tick
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveBee();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
